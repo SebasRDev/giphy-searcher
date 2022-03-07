e64e1f4596e08a66dd4fefd43744749fc4f17478
Sebastian Ramirez <sebasramirez.dev@gmail.com>
2022-03-07 07:03:54 -0500

    all test in GifGridItem

--- src/tests/components/GifGridItem.test.js
+++ src/tests/components/GifGridItem.test.js
@@ -5,10 +5,28 @@ describe('Pruebas en <GifGridItem/>', () => {
   
   const title = "Un titulo"
   const url = "https://localhost/algo.jpg"
+  const wrapper = shallow(<GifGridItem title={title} url={url}/>)
 
   test('Should show component correctly', ()=>{
-    const wrapper = shallow(<GifGridItem title={title} url={url}/>)
     expect(wrapper).toMatchSnapshot();
-
   })
+
+  test('should have a paragraph with the title', ()=>{
+    const p = wrapper.find('p');
+    expect( p.text().trim() ).toBe( title );
+  })
+
+  test('should have th img equal to url and alt od props', () => {
+    const img = wrapper.find('img');
+    expect( img.props().src ).toBe( url );
+    expect( img.props().alt ).toBe( title );
+  })
+
+  test('should have animate__backInUp', ()=>{
+    const div = wrapper.find('div');
+    const className = div.props().className;
+
+    expect( className.includes('animate__backInUp') ).toBe(true);
+  })
+
 })
